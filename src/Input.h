d0c2e37742dacff922f1b6d1b2d3ba28e86bc575
mahaichuan-qq <2805100453@QQ.com>
2025-07-17 14:18:45 +0800

    add roi data

--- src/Input.h
+++ src/Input.h
@@ -12,6 +12,10 @@ typedef struct _NV_INPUT_HEADER {
     uint32_t size; // Size of packet (excluding this field) - Big Endian
     uint32_t magic; // Packet type - Little Endian
     uint32_t traceId;
+    float rectX;
+    float rectY;
+    float rectWidth;
+    float rectHeight;
 } NV_INPUT_HEADER, *PNV_INPUT_HEADER;
 
 #define ENABLE_HAPTICS_MAGIC 0x0000000D
